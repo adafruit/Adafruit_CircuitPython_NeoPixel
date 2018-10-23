86ffb2d755a04d57baf259fe4a10ef78f0ecca24
Kattni Rembor <kattni@adafruit.com>
2018-10-23 14:03:09 -0400

    Updated pin options comment

--- examples/rpi_neopixel_simpletest.py
+++ examples/rpi_neopixel_simpletest.py
@@ -5,7 +5,7 @@ import neopixel
 
 
 # Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
-# NeoPixels must be connected to GPIO10, GPIO12, GPIO18 or GPIO21 to work.
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
 pixel_pin = board.D18
 
 # The number of NeoPixels
